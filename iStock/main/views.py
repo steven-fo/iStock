13abc009a7e88993d5ddcfdb7d635fb2cef8eb16
steven-fo <stevenfo0711@gmail.com>
2023-09-15 01:36:06 +0700

    feat: add form func

--- iStock/main/views.py
+++ iStock/main/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 
+from django.http import HttpResponseRedirect
+from main.forms import ItemForm
+from django.urls import reverse
+from main.models import Item
+
 def show_main(request):
+    item = Item.objects.all()
     context = {
         'name': 'Barang 1',
-        'amount': 999,
-        'description': 'Ini adalah barang 1',
-        'type': 'Ini tipe barang 1'
+        'class': 'PBP C',
+        'items': item
     }
 
     return render(request, "main.html", context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "create_item.html", context) 
