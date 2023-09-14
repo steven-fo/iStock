03919b8ab91b100d61d47c11183d4535ea466c66
steven-fo <stevenfo0711@gmail.com>
2023-09-15 02:04:00 +0700

    feat: add new view funcs

--- iStock/main/views.py
+++ iStock/main/views.py
@@ -5,6 +5,9 @@ from main.forms import ItemForm
 from django.urls import reverse
 from main.models import Item
 
+from django.http import HttpResponse
+from django.core import serializers
+
 def show_main(request):
     item = Item.objects.all()
     context = {
@@ -24,3 +27,23 @@ def create_item(request):
 
     context = {'form': form}
     return render(request, "create_item.html", context) 
+
+def show_html(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("html", data), content_type="application/html")
+
+def show_xml(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
