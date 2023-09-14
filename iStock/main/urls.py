ba87194ae251faef5bca6e9530e9c81b39ca03c0
steven-fo <stevenfo0711@gmail.com>
2023-09-15 01:53:34 +0700

    feat: add button to add new item

--- iStock/main/urls.py
+++ iStock/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_item
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-item', create_item, name="create_item")
 ]
