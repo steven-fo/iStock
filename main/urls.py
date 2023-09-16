1cba3bb72a3332de7c0c6593568823d14c79d5a1
steven-fo <stevenfo0711@gmail.com>
2023-09-16 12:53:07 +0700

    chore: new line typo fix

--- main/urls.py
+++ main/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path('json/', show_json, name="show_json"),
     path('xml/<int:id>/', show_xml_by_id, name="show_xml_by_id"),
     path('json/<int:id>/', show_json_by_id, name="show_json_by_id"),
-
 ]
