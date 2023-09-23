90b27c55b9023d3727cec38e74acce6142d36358
steven-fo <stevenfo0711@gmail.com>
2023-09-23 21:13:17 +0700

    fix: fix parameters

--- main/views.py
+++ main/views.py
@@ -74,7 +74,7 @@ def login_user(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(request, username, password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             response = HttpResponseRedirect(reverse("main:show_main"))
